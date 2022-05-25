09a32e54db2681824440d6c05f055ec317e09e6d
Andtit4 <andtit4@gmail.com>
2022-05-25 17:23:43 +0000

    Adding of the navbar

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,65 @@
 <template>
   <nav>
-    <div class="navbar">
-      <router-link to="/product">Produits</router-link> |
-      <router-link to="/command">Commandes</router-link>
-      <router-link to="/about">A propos</router-link>
+    <div class="navbarContainer">
+      <div class="navbarItems">
+        <li>
+          <router-link to="/about">A propos</router-link>
+        </li>
+        <li>
+          <router-link to="/command">Commandes</router-link> 
+        </li>
+          <li>
+            <router-link to="/product">Produits</router-link> 
+          </li>
+          <!-- <li>
+            <form>
+              <input type="text" placeholder="Rechercher" />
+            </form>
+          </li> -->
+      </div>
     </div>
   </nav>
   <router-view/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+* {
+  background-color: #f5f6fa;
+}
+
+ul {
+  position: fixed;
+  top: 0;
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+  overflow: hidden;
+  width: 60px;
+}
+
+li {
+  padding: 21px;
+  float: right;
+  display: inline;
+}
+
+li a {
+  display: block;
   text-align: center;
-  color: #2c3e50;
+  text-decoration: none;
+  color: #000
 }
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+li a:hover{
+  background-color: transparent;
+  text-decoration: underline;
+  text-underline-position: under;
+  color: #5E4B56
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: #5E4B56;
+  text-decoration: underline;
+  text-underline-position: under
 }
 .vnb .button-red {
     background: #FF3B30;
